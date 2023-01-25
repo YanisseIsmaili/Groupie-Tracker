b4e3c4e7501fc20f28b08405fdb26f1ba3fc2d94
LeYapson <theau.yapi@ynov.com>
2023-01-25 10:42:05 +0100

    localhost

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
+	"path"
+	"time"
 )
 
 func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./view/html")))
-
-	http.HandleFunc("/",IndexHandler)
 
 	fmt.Println("[INFO] - Starting the server...")
 	err := http.ListenAndServe(":8080", nil)
@@ -19,5 +20,28 @@ func main() {
 }
 
 func IndexHandler(w http.ResponseWriter, req *http.Request) {
+	tmpl, err := template.ParseGlob("templates/*")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
+	name := "index.html"
+	if req.URL.Path == "/" {
+		name = "index.html"
+	} else {
+		name = path.Base(req.URL.Path)
+	}
+
+	data := struct {
+		Time time.Time
+	}{
+		Time: time.Now(),
+	}
+
+	if err := tmpl.ExecuteTemplate(w, name, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Println("error", err)
+	}
 }
+
