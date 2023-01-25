b816fec21db06cf4cbcc11c7d57041ae3ba60c78
LeYapson <theau.yapi@ynov.com>
2023-01-25 10:31:51 +0100

    uwuntu

---
+++ controller/htmlfiles.go
@@ -0,0 +1,3 @@
+package groupie
+
+var IndexPage = []string{"./view/html/index.html", "./view/html/templates/head.html"}
--- main.go
+++ main.go
@@ -6,9 +6,18 @@ import (
 )
 
 func main() {
+
+	http.Handle("/", http.FileServer(http.Dir("./view/html")))
+
+	http.HandleFunc("/",IndexHandler)
+
 	fmt.Println("[INFO] - Starting the server...")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("[ERROR] - Server could not start properly.\n ", err)
 	}
+}
+
+func IndexHandler(w http.ResponseWriter, req *http.Request) {
+
 }
