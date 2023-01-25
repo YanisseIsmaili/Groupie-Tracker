ccee88ca7b4315d522ca89de6db26fc0271a2e55
yaourt <yanisse.ismaili@ynov.com>
2023-01-25 10:08:10 +0100

    rasciste

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
-package groupie
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("[INFO] - Starting the server...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("[ERROR] - Server could not start properly.\n ", err)
+	}
+}
