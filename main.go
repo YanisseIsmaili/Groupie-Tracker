440a451ae091e1ae9c8c04330a0f999f79c5775b
LeYapson <theau.yapi@ynov.com>
2023-03-10 10:26:09 +0100

    bobdell

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./view/html")))
 	http.HandleFunc("/artist.html", handleArtist)
+	http.HandleFunc("/getJson.js", handleArtist)
 	fmt.Println("[INFO] server starting at port 8080.")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
