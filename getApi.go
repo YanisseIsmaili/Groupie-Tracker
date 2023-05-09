eba8ef8b413be835b78f0b067c393eb87a9eb3f8
LeYapson <theau.yapi@ynov.com>
2023-05-09 19:41:27 +0200

    groupie

--- getApi.go
+++ getApi.go
@@ -20,6 +20,12 @@ type Artist struct {
 	Relations    string 
 }
 
+type Location struct {
+	Id           int
+	Locations    []string
+	ConcertDates string
+}
+
 
 
 // ? this function reads the artists API from website and prints the relevant information
@@ -67,4 +73,51 @@ func getArtists(filePath string) ([]Artist, error) {
 		return nil, fmt.Errorf("erreur lors du décodage du JSON : %w", err)
 	}
 	return artists, nil
+}
+
+// ? this function reads the locations API from website and prints the relevant information
+func handleLocation(w http.ResponseWriter, r *http.Request) {
+	locations, err := getLocations("https://groupietrackers.herokuapp.com/api/locations")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//* Parse the HTML template
+	tmpl, err := template.ParseFiles("./view/html/location.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//* Executes the template by passing the artists' data
+	tmpl.Execute(w, locations)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// * Get the artists from the API
+func getLocations(filePath string) ([]Location, error) {
+	//* Opening the JSON page online
+	resp, err := http.Get(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("erreur lors de l'ouverture du fichier : %w", err)
+	}
+	defer resp.Body.Close()
+
+	//* Reading the contents of the link
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("erreur lors de la lecture du fichier : %w", err)
+	}
+
+	//* Decoding JSON content in an Artist slice
+	var locations []Location
+	json.Unmarshal(content, &locations)
+	if err != nil {
+		return nil, fmt.Errorf("erreur lors du décodage du JSON : %w", err)
+	}
+	return locations, nil
 }
