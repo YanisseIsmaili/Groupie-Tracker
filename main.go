a6eaac3109790ddcd4163497122f39472ef4ee57
théau Yapi <theau.yapi@ynov.com>
2023-05-09 21:14:31 +0200

    a detruire

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/artist.html", handleArtist)
-	http.HandleFunc("/location.html", handleLocation)
-	fmt.Println("[INFO] server starting at port 8080.")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/artists", handleArtist)
+	http.HandleFunc("/locations", handleLocation)
+	fmt.Println("Listening on localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
