c7cce66c1e196ee25c87b3f739fb0743f91900c5
LeYapson <theau.yapi@ynov.com>
2023-05-11 11:00:40 +0200

    location et date sont en local pour la présentation oklm

--- getApi.go
+++ getApi.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 type Artist struct {
@@ -26,7 +27,10 @@ type Location struct {
 	ConcertDates string
 }
 
-
+type Date struct {
+	Id    int
+	Dates []string
+}
 
 // ? this function reads the artists API from website and prints the relevant information
 func handleArtist(w http.ResponseWriter, r *http.Request) {
@@ -75,9 +79,9 @@ func getArtists(filePath string) ([]Artist, error) {
 	return artists, nil
 }
 
-// ? this function reads the locations API from website and prints the relevant information
+//? this function reads the location API from a local file and prints the relevant information
 func handleLocation(w http.ResponseWriter, r *http.Request) {
-	locations, err := getLocations("https://groupietrackers.herokuapp.com/api/locations")
+	locations, err := getLocation("./data/locations.json")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -90,34 +94,83 @@ func handleLocation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//* Executes the template by passing the artists' data
-	tmpl.Execute(w, locations)
+	//* Executes the template by passing the location data
+	err = tmpl.Execute(w, locations)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
-// * Get the artists from the API
-func getLocations(filePath string) ([]Location, error) {
-	//* Opening the JSON page online
-	resp, err := http.Get(filePath)
+//* Get the location from the API
+func getLocation(filePath string) ([]Location, error) {
+	//* Opening the local JSON file
+	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, fmt.Errorf("erreur lors de l'ouverture du fichier : %w", err)
 	}
-	defer resp.Body.Close()
+	defer file.Close()
 
-	//* Reading the contents of the link
-	content, err := ioutil.ReadAll(resp.Body)
+	//* Reading the contents of the file
+	content, err := ioutil.ReadAll(file)
 	if err != nil {
 		return nil, fmt.Errorf("erreur lors de la lecture du fichier : %w", err)
 	}
 
-	//* Decoding JSON content in an Artist slice
+	//* Decoding JSON content in a Location slice
 	var locations []Location
-	json.Unmarshal(content, &locations)
+	err = json.Unmarshal(content, &locations)
 	if err != nil {
 		return nil, fmt.Errorf("erreur lors du décodage du JSON : %w", err)
 	}
+
 	return locations, nil
+}
+
+//? this function reads the date API from a local file and prints the relevant information
+func handleDate(w http.ResponseWriter, r *http.Request) {
+	dates, err := getDates("./data/dates.json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//* Parse the HTML template
+	tmpl, err := template.ParseFiles("./view/html/date.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//* Executes the template by passing the date data
+	err = tmpl.Execute(w, dates)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+//* Get the date from the API
+func getDates(filePath string) ([]Date, error) {
+	//* Opening the local JSON file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("erreur lors de l'ouverture du fichier : %w", err)
+	}
+	defer file.Close()
+
+	//* Reading the contents of the file
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("erreur lors de la lecture du fichier : %w", err)
+	}
+
+	//* Decoding JSON content in a date slice
+	var dates []Date
+	err = json.Unmarshal(content, &dates)
+	if err != nil {
+		return nil, fmt.Errorf("erreur lors du décodage du JSON : %w", err)
+	}
+
+	return dates, nil
 }
